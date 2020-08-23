5d9fb4f8394dd9143c29ea0868bf4d500d418cea
RafalMazuchowski <mazuchowski.r@gmail.com>
2020-08-23 17:41:46 +0200

    Project portfolio - an application solving Sudoku (16.8) ver. 3

--- kodilla-sudoku/src/main/java/com/kodilla/sudoku/SudokuGame.java
+++ kodilla-sudoku/src/main/java/com/kodilla/sudoku/SudokuGame.java
@@ -8,10 +8,32 @@ import java.util.regex.Pattern;
 public class SudokuGame {
     SudokuBoard sudokuBoard;
     Scanner scan = new Scanner(System.in);
+    int[][][] sections = {
+            {{0,0}, {0,1}, {0,2}, {1,0}, {1,1}, {1,2}, {2, 0}, {2,1}, {2,2}},
+            {{0,3}, {0,4}, {0,5}, {1,3}, {1,4}, {1,5}, {2, 3}, {2,4}, {2,5}},
+            {{0,6}, {0,7}, {0,8}, {1,6}, {1,7}, {1,8}, {2, 6}, {2,7}, {2,8}},
+            {{3,0}, {3,1}, {3,2}, {4,0}, {4,1}, {4,2}, {5, 0}, {5,1}, {5,2}},
+            {{3,3}, {3,4}, {3,5}, {4,3}, {4,4}, {4,5}, {5, 3}, {5,4}, {5,5}},
+            {{3,6}, {3,7}, {3,8}, {4,6}, {4,7}, {4,8}, {5, 6}, {5,7}, {5,8}},
+            {{6,0}, {6,1}, {6,2}, {7,0}, {7,1}, {7,2}, {8, 0}, {8,1}, {8,2}},
+            {{6,3}, {6,4}, {6,5}, {7,3}, {7,4}, {7,5}, {8, 3}, {8,4}, {8,5}},
+            {{6,6}, {6,7}, {6,8}, {7,6}, {7,7}, {7,8}, {8, 6}, {8,7}, {8,8}},
+    };
 
 
     public SudokuGame() {
         int[][] board = {
+                {-1, 9, -1, -1, 4, -1, 8, -1, -1},
+                {5, -1, 1, 8, -1, -1, -1, -1, -1},
+                {4, -1, -1, -1, -1, -1, 7, -1, -1},
+                {8, 3, 5, -1, 2, -1, -1, -1, -1},
+                {1, -1, 7, -1, 3, -1, -1, -1, -1},
+                {-1, 2, 9, -1, -1, -1, -1, -1, -1},
+                {-1, 8, -1, -1, -1, 9, 2, 3, -1},
+                {-1, -1, -1, 2, -1, -1, -1, -1, -1},
+                {-1, -1, 2, -1, 7, 5, 4, 9, -1},
+        };
+        /*int[][] board = {
                 {-1, 2, -1, 5, -1, 1, -1, 9, -1},
                 {8, -1, -1, 2, -1, 3, -1, -1, 6},
                 {-1, 3, -1, -1, 6, -1, -1, 7, -1},
@@ -21,13 +43,35 @@ public class SudokuGame {
                 {-1, 9, -1, -1, 3, -1, -1, 8, -1},
                 {2, -1, -1, 8, -1, 4, -1, -1, 7},
                 {-1, 1, -1, 9, -1, 7, -1, 6, -1},
-        };
+        };*/
+       /* int[][] board = {
+                {-1, 2, -1, 5, -1, 1, -1, 9, -1},
+                {8, -1, -1, 2, -1, 3, -1, -1, 6},
+                {-1, 3, -1, -1, 6, -1, -1, 7, -1},
+                {-1, -1, 1, 3, 8, -1, 6, -1, -1},
+                {5, 4, -1, -1, 2, -1, -1, 1, 9},
+                {-1, -1, 2, -1, 4, -1, 7, -1, -1},
+                {-1, 9, -1, -1, 3, -1, -1, 8, -1},
+                {2, -1, -1, 8, -1, 4, -1, -1, 7},
+                {-1, 1, -1, 9, -1, 7, -1, 6, -1},
+        };*/
+        /*int[][] board = {
+                {4, 2, 6, 5, 7, 1, 3, 9, 8},
+                {8, 5, 7, 2, 9, 3, 1, 4, 6},
+                {1, 3, 9, 4, 6, 8, 2, 7, 5},
+                {9, 7, 1, 3, 8, 5, 6, 2, 4},
+                {5, 4, 3, 7, 2, 6, 8, 1, 9},
+                {6, 8, 2, 1, 4, 8, 7, 5, 3},
+                {7, 9, 4, 6, 3, 2, 5, 8, 1},
+                {2, 6, 5, 8, 1, 4, 9, 3, 7},
+                {3, -1, -1, 9, 5, 7, -1, 6, 2}
+        };*/
         this.sudokuBoard = new SudokuBoard(board);
         System.out.println(sudokuBoard.toString());
     }
 
 
-    public boolean contains(int value, ArrayList<SudokuElement> sudokuElements) {
+    public boolean alreadyTaken(int value, ArrayList<SudokuElement> sudokuElements) {
         for (SudokuElement sudokuElement : sudokuElements) {
             if (sudokuElement.value == value) {
                 return true;
@@ -36,13 +80,22 @@ public class SudokuGame {
         return false;
     }
 
-    public boolean canInsert(int value, int sudokuElementIndex, ArrayList<SudokuElement> sudokuElements) {
+    public boolean isOnlyOptionForSection(int value, int sudokuElementIndexToOmit, ArrayList<SudokuElement> sudokuElements) {
         for (int i = 0; i < 9; i++) {
-            if (i != sudokuElementIndex && sudokuElements.get(i).value == SudokuElement.EMPTY && sudokuElements.get(i).containsPossible(value)) {
+            if (i != sudokuElementIndexToOmit && sudokuElements.get(i).value == SudokuElement.EMPTY && sudokuElements.get(i).containsPossible(value)) {
                 return false;
             }
         }
         return true;
+    }
+
+    public boolean tryInsert(int value, SudokuElement sudokuElement, ArrayList<SudokuElement> sudokuElements) throws Exception {
+        if (!alreadyTaken(value, sudokuElements)) {
+            sudokuElement.value = value;
+            return true;
+        } else {
+            throw new Exception("Unsolvable!");
+        }
     }
 
     public boolean resolveSection(ArrayList<SudokuElement> sudokuElements) throws Exception {
@@ -51,21 +104,16 @@ public class SudokuGame {
             SudokuElement sudokuElement = sudokuElements.get(s);
             if (sudokuElement.value == SudokuElement.EMPTY) {
                 for (int i = 0; i < 9; i++) {
-                    int pos = sudokuElement.possibleValues[i];
-                    if (pos != 0) {
-
-                        if (contains(pos, sudokuElements)) {
+                    int possibleValue = sudokuElement.possibleValues[i];
+                    if (possibleValue != 0) {
+                        if (alreadyTaken(possibleValue, sudokuElements)) {
                             sudokuElement.possibleValues[i] = 0;
                             actionPerformed = true;
-                            if (sudokuElement.hasChosenOne()) {
-                                if (canInsert(sudokuElement.chosenOne(), s, sudokuElements)) {
-                                    sudokuElement.value = sudokuElement.chosenOne();
-                                    break;
-                                } else throw new Exception("Unsolvable!");
+                            if (sudokuElement.hasChosenOne() && tryInsert(sudokuElement.chosenOne(), sudokuElement, sudokuElements)) {
+                                break;
                             }
                         } else {
-                            if (canInsert(pos, s, sudokuElements)) {
-                                sudokuElement.value = pos;
+                            if (isOnlyOptionForSection(possibleValue, s, sudokuElements) && tryInsert(possibleValue, sudokuElement, sudokuElements)) {
                                 actionPerformed = true;
                                 break;
                             }
@@ -81,7 +129,10 @@ public class SudokuGame {
         boolean actionPerformed = false;
         //horizontal
         for (SudokuRow sudokuRow : this.sudokuBoard.sudokuRows) {
-            actionPerformed = resolveSection(sudokuRow.sudokuElements);
+            boolean actionPerformedHorizontal = resolveSection(sudokuRow.sudokuElements);
+            if (actionPerformedHorizontal){
+                actionPerformed = true;
+            }
         }
 
         //vertical
@@ -90,19 +141,21 @@ public class SudokuGame {
             for (SudokuRow sudokuRow : this.sudokuBoard.sudokuRows) {
                 sudokuElements.add(sudokuRow.sudokuElements.get(i));
             }
-            actionPerformed = resolveSection(sudokuElements);
+            boolean actionPerformedVertical = resolveSection(sudokuElements);
+            if (actionPerformedVertical){
+                actionPerformed = true;
+            }
         }
 
         //squares
-        for (int m = 0; m < 3; m++) {
-            for (int n = 0; n < 3; n++) {
-                ArrayList<SudokuElement> sudokuElements = new ArrayList<>();
-                for (int i = m * 3; i < m * 3 + 3; i++) {
-                    for (int j = n * 3; j < n * 3 + 3; j++) {
-                        sudokuElements.add(this.sudokuBoard.sudokuRows.get(i).sudokuElements.get(j));
-                    }
-                }
-                actionPerformed = resolveSection(sudokuElements);
+        for(int[][] section:sections) {
+            ArrayList<SudokuElement> sudokuElements = new ArrayList<>();
+            for(int[] cell:section) {
+                sudokuElements.add(this.sudokuBoard.sudokuRows.get(cell[0]).sudokuElements.get(cell[1]));
+            }
+            boolean actionPerformedSquares = resolveSection(sudokuElements);
+            if (actionPerformedSquares){
+                actionPerformed = true;
             }
         }
         return actionPerformed;
@@ -112,7 +165,7 @@ public class SudokuGame {
         Pattern pattern = Pattern.compile("([1-9]),([1-9]),([1-9])");
         System.out.println("INSERT INITIAL VALUES...");
         String input = scan.nextLine();
-        while (!input.equals("SUDOKU")) {
+        while (!input.equals("S")) {
             Matcher matcher = pattern.matcher(input);
             if (matcher.matches()) {
                 System.out.println("Row: " + matcher.group(1) + ", Element: " + matcher.group(2));
@@ -134,8 +187,9 @@ public class SudokuGame {
         while (actionPerformed) {
             actionPerformed = iterate();
         }
+        System.out.println(sudokuBoard.toString());
         if (sudokuBoard.isSolved()) {
-            System.out.println(sudokuBoard.toString());
+            System.out.println("Solved!");
         } else {
             System.out.println("Guessing...");
         }
